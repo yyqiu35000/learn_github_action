aa78f2cbe659411ed688d3a081ba9573eca31bb5
yyqiu35000 <3500063778@qq.com>
2025-11-10 14:14:09 +0800

    add new test

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -7,10 +7,55 @@ import org.junit.jupiter.api.Test;
 class MainTest {
 
 	@Test
-	void test() {
+	void testAddPositiveNumbers() {
 		Main main = new Main();
 		int result = main.add(1, 1);
 		assertEquals(2, result);
+	}
+
+	@Test
+	void testAddWithFirstNegative() {
+		Main main = new Main();
+		assertThrows(IllegalArgumentException.class, () -> main.add(-1, 5));
+	}
+
+	@Test
+	void testAddWithSecondNegative() {
+		Main main = new Main();
+		assertThrows(IllegalArgumentException.class, () -> main.add(5, -1));
+	}
+
+	@Test
+	void testAddWithBothNegative() {
+		Main main = new Main();
+		assertThrows(IllegalArgumentException.class, () -> main.add(-1, -1));
+	}
+
+	@Test
+	void testAddWithIntegerOverflow() {
+		Main main = new Main();
+		assertThrows(ArithmeticException.class, () -> main.add(Integer.MAX_VALUE, 1));
+	}
+
+	@Test
+	void testAddMaxIntWithZero() {
+		Main main = new Main();
+		int result = main.add(Integer.MAX_VALUE, 0);
+		assertEquals(Integer.MAX_VALUE, result);
+	}
+
+	@Test
+	void testAddZeroWithMaxInt() {
+		Main main = new Main();
+		int result = main.add(0, Integer.MAX_VALUE);
+		assertEquals(Integer.MAX_VALUE, result);
+	}
+
+	@Test
+	void testAddLargeNumbersNoOverflow() {
+		Main main = new Main();
+		int result = main.add(Integer.MAX_VALUE / 2, Integer.MAX_VALUE / 2);
+		assertEquals(Integer.MAX_VALUE - 1, result);
 	}
 
 }
